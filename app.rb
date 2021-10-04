33fd3f0eecc774a37081c8ebfb3b694883a340fc
arjun <vbarjun1009@gmail.com>
2021-10-03 17:13:16 -0700

    GetFiles

--- app.rb
+++ app.rb
@@ -1,7 +1,24 @@
 require 'sinatra'
+require 'google/cloud/storage'
+require 'digest'
+require 'json'
 
 get '/' do
   redirect to("/files/"), 302
+end
+
+get '/files/' do
+  storage = Google::Cloud::Storage.new(project_id: 'cs291a')
+  bucket = storage.bucket 'cs291project2', skip_lookup: true
+  all_files = bucket.files
+  array_files = []
+  all_files.all do |file|
+    array_files.append(file.name.downcase)
+  end
+  array_files.sort!
+
+  #puts array_files.to_json
+  array_files.to_json
 end
 
 post '/' do
