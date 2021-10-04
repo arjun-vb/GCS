fd53655c254910e05ffa4df8a24813bd5ad4c1da
arjun <vbarjun1009@gmail.com>
2021-10-03 22:21:36 -0700

    postFile

--- app.rb
+++ app.rb
@@ -16,9 +16,47 @@ get '/files/' do
     array_files.append(file.name.downcase)
   end
   array_files.sort!
+  status  201
+  body array_files.to_json
+end
 
-  #puts array_files.to_json
-  array_files.to_json
+get '/files/:digest' do
+  storage = Google::Cloud::Storage.new(project_id: 'cs291a')
+  bucket = storage.bucket 'cs291project2', skip_lookup: true
+  file = bucket.file digest
+  
+  puts file.name
+end 
+
+post '/files/' do
+  file = params[:file]
+
+  if request.content_length.to_i > 1048576
+    status 422
+  elsif file != nil and file['tempfile'] != nil
+    txt = open(file['tempfile']).read
+    sha256 = Digest::SHA256.hexdigest txt
+
+    newdigest = sha256[0,2] + "/" + sha256[2,2] + "/" + sha256[4,60]
+    puts sha256
+    puts newdigest
+
+    storage = Google::Cloud::Storage.new(project_id: 'cs291a')
+    bucket = storage.bucket 'cs291project2', skip_lookup: true
+    exist = bucket.file newdigest
+
+    if exist == nil
+      #bucket.createfile file['tempfile'], path = newdigest, content_type = file['type']
+      response = { uploaded: sha256 }.to_json
+      status  201 
+      body response
+    else
+      status 409
+    end
+  else
+    status 422
+  end
+
 end
 
 post '/' do
