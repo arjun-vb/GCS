b5f5806e403237385c99538a6ad83a5d12ba9166
arjun <vbarjun1009@gmail.com>
2021-10-04 16:09:32 -0700

    test

--- app.rb
+++ app.rb
@@ -15,11 +15,12 @@ get '/files/' do
   all_files = bucket.files
   array_files = []
   
-  digest_format = /[0-9a-z]{2}\/[0-9a-z]{2}\/[0-9a-z]{60}/
+  digest_format = /\A[0-9a-f]{2}\/[0-9a-f]{2}\/[0-9a-f]{60}\Z/
 
   all_files.all do |file|
     if (!!file.name.downcase.match(digest_format))
-      array_files.append(file.name.downcase)
+      filename = file.name.downcase[0,2] + file.name.downcase[3,2] + file.name.downcase[6,60]
+      array_files.append(filename)
     end
   end
   array_files.sort!
@@ -30,19 +31,19 @@ end
 
 
 get '/files/:digest' do
-  digest_raw = /[0-9a-z]{64}/
-  digest = params['digest']
+  digest_raw = /\A[0-9a-f]{64}\Z/
+  digest = params['digest'].downcase
 
-  if (!!digest.downcase.match(digest_raw))
+  if (!!digest.match(digest_raw))
     storage = Google::Cloud::Storage.new(project_id: 'cs291a')
     bucket = storage.bucket 'cs291project2', skip_lookup: true
     newdigest = digest[0,2] + "/" + digest[2,2] + "/" + digest[4,60]
     file = bucket.file newdigest
     
     if (file != nil)
-      body file.download.read
-      content_type file.content_type
       status 200
+      body file.download.read
+      content_type file.content_type      
     else 
       status 404
     end
@@ -55,11 +56,16 @@ end
 
 post '/files/' do
   file = params[:file]
+  if file != nil
+    filetemp = file['tempfile']
+  end
 
-  if request.content_length.to_i > 1048576
-    status 422
-  elsif file != nil and file['tempfile'] != nil
-    txt = open(file['tempfile']).read
+  if file != nil and filetemp != nil and File.file?(filetemp)
+    if filetemp.size > 1048576
+      status 422
+      return
+    end
+    txt = open(filetemp).read
     sha256 = Digest::SHA256.hexdigest txt
 
     newdigest = sha256[0,2] + "/" + sha256[2,2] + "/" + sha256[4,60]
@@ -69,7 +75,7 @@ post '/files/' do
     exist = bucket.file newdigest
 
     if exist == nil
-      bucket.create_file file['tempfile'], path = newdigest, content_type: file['type']
+      bucket.create_file filetemp, path = newdigest, content_type: file['type']
       response = { uploaded: sha256 }.to_json
       status  201 
       body response
@@ -84,17 +90,20 @@ end
 
 
 delete '/files/:digest' do
-  digest_raw = /[0-9a-z]{64}/
-  digest = params['digest']
-  if (!!digest.downcase.match(digest_raw))
+  digest_raw = /\A[0-9a-f]{64}\Z/
+  digest = params['digest'].downcase
+
+  if (!!digest.match(digest_raw))
+    status 200
     storage = Google::Cloud::Storage.new(project_id: 'cs291a')
     bucket = storage.bucket 'cs291project2', skip_lookup: true
     newdigest = digest[0,2] + "/" + digest[2,2] + "/" + digest[4,60]
     file = bucket.file newdigest
+
     if (file != nil)
       file.delete
     end
-    status 200
+
   else
     status 422
   end
